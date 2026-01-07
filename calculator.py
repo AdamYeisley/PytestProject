8d89b2e5b7b48ee1cae5010c53380c082dfee37a
Adam Yeisley <ovo.adam.yeisley.ovo@gmail.com>
2026-01-06 19:21:04 -0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# This will be a simple class with arithmetic math methods
+# To write tests against
+
+def add(x, y):
+   return x + y
+
+def subtract(x, y):
+   return x - y
+
+def multiply(x, y):
+   return x * y
+
+def divide(x, y):
+   if y == 0:
+      raise ValueError("Cannot divide by zero")
+   return x / y
